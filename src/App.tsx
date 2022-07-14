31a4654c3e8fb1720572ca62de536b7a1facac31
Pawel Sawicki <public@psawicki.de>
2022-07-14 14:54:42 +0200

    useEffect for loading books

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { BookList } from './components/BookList';
 import { Book } from './domain/books';
 
 function App() {
+  const baseUrl = 'http://localhost:4730';
   const [books, setBooks] = useState<Book[] | null>(null);
+  useEffect(() => {
+    const fetchBooks = async (baseUrl: string) => {
+      const response = await fetch(`${baseUrl}/books`);
+      const _books = await response.json();
+      setBooks(_books);
+    };
+
+    fetchBooks(baseUrl);
+  }, [baseUrl]);
+  // Load books here, using useEffect and fetch() api ...
   return <div className="App">{books ? <BookList books={books} /> : <span>Loading...</span>}</div>;
 }
 
--- src/data/books.ts
+++
@@ -1,7 +0,0 @@
-import { Book } from '../domain/books';
-
-export const books: Book[] = [
-  { title: 'A first Book' },
-  { title: 'Another awesome Book' },
-  { title: 'Learn React - the full book' },
-];
